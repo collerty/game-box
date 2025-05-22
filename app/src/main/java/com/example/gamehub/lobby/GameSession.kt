f25af7a24fd301c7bb0fb0442676d6004e96b4a0
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2025-05-22 19:32:26 +0200

    Fixes

--- app/src/main/java/com/example/gamehub/lobby/GameSession.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gamehub.lobby
-
-import kotlinx.coroutines.flow.Flow
-
-interface GameSession<MOVE, STATE> {
-    val stateFlow: Flow<STATE>
-    suspend fun sendMove(move: MOVE)
-    suspend fun close()
-}
---
+++ app/src/main/java/com/example/gamehub/lobby/model/Move.kt
@@ -0,0 +1,2 @@
+package com.example.gamehub.lobby.model 
+

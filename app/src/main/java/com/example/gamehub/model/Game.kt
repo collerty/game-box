89a4f8145c45740b55f014efd826f87ab79f6b68
NagyMilan111 <milan.nagy@student.nhlstenden.com>
2025-06-18 13:56:10 +0200

    feat(UI): added backgrounds for the gamecards for ohpardon and spaceinvaders

--- app/src/main/java/com/example/gamehub/model/Game.kt
+++ app/src/main/java/com/example/gamehub/model/Game.kt
@@ -10,7 +10,7 @@ sealed class Game(
     val imageRes: Int,
 ) {
     object BattleShips  : Game("battleships", "BattleShips",  R.drawable.icon_battleships, online = true, R.drawable.battleships_menu)
-    object OhPardon     : Game("ohpardon",   "Oh Pardon",    R.drawable.ic_ohpardon,    online = true, R.drawable.doodle_menu)
+    object OhPardon     : Game("ohpardon",   "Oh Pardon",    R.drawable.ic_ohpardon,    online = true, R.drawable.ohpardon_gamecard)
     object WhereAndWhen : Game("whereandwhen", "Where & When", R.drawable.ic_where_and_when, online = true, R.drawable.doodle_menu)
     object Codenames    : Game("codenames",  "Codenames",    R.drawable.ic_codenames,   online = true, R.drawable.codenames_preview)
     object Triviatoe : Game("triviatoe", "Triviatoe", R.drawable.icon_triviatoe, online = true, R.drawable.triviatoe_menu)
@@ -20,7 +20,7 @@ sealed class Game(
     object Spy          : Game("spy",        "Spy",          R.drawable.ic_spy,         online = false, R.drawable.spy_preview)
     object JorisJump    : Game("jorisjump",  "Joris Jump",   R.drawable.ic_jorisjump,   online = false, R.drawable.doodle_menu)
     object ScreamoSaur  : Game("screamosaur","Scream-O-Saur",R.drawable.ic_screamosaur, online = false, R.drawable.doodle_menu)
-    object SpaceInvaders  : Game("spaceinvaders","Space Invaders",R.drawable.ic_spaceinvaders, online = false, R.drawable.doodle_menu)
+    object SpaceInvaders  : Game("spaceinvaders","Space Invaders",R.drawable.ic_spaceinvaders, online = false, R.drawable.spaceinvaders_gamecard)
 
     object MemoryMatch  : Game("memoryMatching", "Memory Match", R.drawable.ic_memory_match, online = false, R.drawable.doodle_menu) // New Game
 

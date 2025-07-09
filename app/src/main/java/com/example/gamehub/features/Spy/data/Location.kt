2496e665f6a67eb8ad19e3ad86d5307d10cda0cc
unknown <nhlsviatoslav@gmail.com>
2025-07-09 21:48:35 +0200

    refactor(spygame): folder structure

--- app/src/main/java/com/example/gamehub/features/Spy/data/Location.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gamehub.features.spy.data
-
-data class Location(
-    val name: String,
-    val description: String
-) 
---
+++ app/src/main/java/com/example/gamehub/features/Spy/model/Location.kt
@@ -0,0 +1,6 @@
+package com.example.gamehub.features.spy.data
+
+data class Location(
+    val name: String,
+    val description: String
+) 
